148972fdfba11ff0c414463c3a38609c2b504656
Nooppphsh77 <yarb75006@gmail.com>
2024-08-28 18:15:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -14,52 +14,20 @@ global.authFile = `MysticSession`;
 global.isBaileysFail = false
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['573012482597', '💫 Mystic - OfcDiego', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['5493794297363', '💫 Mystic - Gabriel Ofc', true], 
-  ['393518398856', '💫 Mystic - Tester Fabri115💫', true],
-  ['212781160203', '💫 Mystic - Saad 💫', true], 
-  ['5217778573223', '💫 Mystic - Sarethh 💫', true],
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['5493795319019'],
-  ['573147616444'], 
-  ['5219991402134'],
-  ['5219996125657'],
-  ['5492215034412'],
-  ['5492964650915'],
-  ['5218444937407']
+  ['201203024198', '👑  Đᴅ ẽ Ќ Ś Т Λ Ŕ 👑', true],
+  ['201503742544', '💫  Đᴅ ẽ Ќ Ś Т Λ Ŕ 2 💫', true],
+  
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['201503742544'];
+global.prems = ['201503742544'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.author = 'Đᴅ ẽ Ќ Ś Т Λ Ŕ - Bot';
+global.wm = 'Đᴅ ẽ Ќ Ś Т Λ Ŕ - Bot';
+global.titulowm = 'Đᴅ ẽ Ќ Ś Т Λ Ŕ - Bot';
+global.titulowm2 = `Đᴅ ẽ Ќ Ś Т Λ Ŕ - Bot`
+global.igfg = 'Đᴅ ẽ Ќ Ś Т Λ Ŕ - Bot';
 global.wait = '*_[ ⏳ ] Cargando..._*';
 
 global.imagen1 = fs.readFileSync('./src/assets/images/menu/languages/es/menu.png');
